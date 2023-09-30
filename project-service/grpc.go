96d7472629f2f3365ea84d252261f17a3fe0cf49
Noah Wilderom <wilderomnoah@gmail.com>
2023-10-01 00:13:50 +0200

    wip

--- project-service/grpc.go
+++ project-service/grpc.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"github.com/Noah-Wilderom/cloud-services/shared-grpc/projects"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type Config struct{}
+
+func (app *Config) gRPCListen() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", gRPCPort))
+	if err != nil {
+		log.Fatalln("Failed to listen for gRPC:", err)
+	}
+
+	s := grpc.NewServer()
+
+	projects.RegisterQueueListenerServiceServer(s, &QueueListenerServer{})
+	log.Println("gRPC Server started on port", gRPCPort)
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalln("Failed to listen for gRPC:", err)
+	}
+}
--- project-service/main.go
+++ project-service/main.go
@@ -1,1 +1,5 @@
 package main
+
+const (
+	gRPCPort = 5004
+)
